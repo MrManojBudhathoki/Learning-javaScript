e4745690249c066c1babe315ea6d74a860f0c902
Manoj <manojbudhathoki265@gmail.com>
2024-02-12 22:23:28 +1100

    Learning JavaScript

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let cartquantity = 0;
+
+document.getElementById("showQuantity").onclick = function(){
+    console.log(`Cart quantity: ${cartquantity}`);
+}
+document.getElementById("add").onclick = function(){
+    cartquantity = cartquantity + 1;
+    console.log(`Cart quantity: ${cartquantity}`);
+}
+document.getElementById("add2").onclick = function(){
+    cartquantity = cartquantity + 2;
+    console.log(`Cart quantity: ${cartquantity}`);
+}
+document.getElementById("add3").onclick = function(){
+    cartquantity = cartquantity + 3;
+    console.log(`Cart quantity: ${cartquantity}`)
+}
+document.getElementById("reset").onclick = function(){
+    window.alert("Do you want to reset all that you have done so far?")
+}
