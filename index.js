ed0ebca52999d10c3bbeb359ac762980297a843f
Manoj <manojbudhathoki265@gmail.com>
2024-03-02 13:54:58 +1100

    TempConverter

--- index.js
+++ index.js
@@ -1,78 +1,32 @@
-//switch = statement that examines a value
-//         for a match against many case clauses
-//         More efficient that many "else if" statements
+let temp;
 
-/*
-This is done without using switch(Just to know the differences between if and switch.)
-let grade = "A";
+document.getElementById("submitButton").onclick = function(){
 
-if(grade == "A"){
-    console.log("You did great!");
+    temp = document.getElementById("textBox").value;
+    temp = Number(temp);
+
+
+    if(document.getElementById("cButton").checked){
+        temp = tocelsius(temp);
+        temp = Math.round(temp);
+        document.getElementById("tempLabel").innerHTML = temp + "°c"
+    }
+
+    else if(document.getElementById("fButton").checked){
+        temp = toFarenhehit(temp);
+        temp = Math.round(temp);
+        document.getElementById("tempLabel").innerHTML = temp + "°c"
+    }
+
+    else{
+        document.getElementById("tempLabel").innerHTML = "Select the unit first."
+    }
 }
-else if(grade == "B"){
-    console.log("You did good!")
+
+function tocelsius(temp){
+    return (temp - 32) * (5/9);
 }
-else if(grade == "C"){
-    console.log("You did okay!");
-}
-else if(grade == "D"){
-    console.log("You passed barely!");
-}
-else if(grade == "F"){
-    console.log("You failed this time!");
-}
-else{
-    console.log("The letter that you have entered is not a grade!");
-}*/
 
-/*let grade = "A";//This is done using switch and this program do the same thing as the above only difference is this is using the switch property and above one is using if else statements.
-
-
-switch(grade){
-    case "A":
-        console.log("You did great!");
-        break;
-    case "B":
-        console.log("You did good!");
-        break;
-    case "C":
-        console.log("You did okay!")
-        break;
-    case "D":
-        console.log("You passed barely!");
-        break;
-    case "F":
-        console.log("You failed");
-        break;
-    default:
-        console.log("There is no such letter in grading system.")            
-
-}*/
-//Let's say we are doing the same programming like above but this time we will check by using numbers instead of letters.
-let grade = 80;
-
-switch(true){
-    case grade > 100:
-        console.log("There is no such grade available!");
-        break;
-    case grade >= 90:
-        console.log("You did great!");
-        break;
-    case grade >= 80:
-        console.log("You did good!");
-        break;
-    case grade >= 70:
-        console.log("You did okay!");
-        break;
-    case grade >= 60:
-        console.log("You passed .... barely!");
-        break;
-    case grade >= 50:
-        console.log("You just passed!")
-        break;
-    case grade < 50:
-        console.log("You failed!");
-        break;
-    default:
-        console.log("There is no such grade available!");                       
+function toFarenhehit(temp){
+    return temp * 9 / 5 + 32;
 }
